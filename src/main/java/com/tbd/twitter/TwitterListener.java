bf852dbc9625fb4358822ef6c60044c814a49995
Roberto Solar <roberto.solar.gallardo@gmail.com>
2018-11-14 14:33:05 -0300

    bag of words

--- src/main/java/com/tbd/twitter/TwitterListener.java
+++ src/main/java/com/tbd/twitter/TwitterListener.java
@@ -1,67 +1,78 @@
 package com.tbd.twitter;
 
 import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
-
 import twitter4j.FilterQuery;
-import twitter4j.JSONException;
-import twitter4j.JSONObject;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
 import twitter4j.TwitterStream;
-import twitter4j.json.DataObjectFactory;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.Resource;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Configurable
 public class TwitterListener {
 	@Autowired
+	private ResourceLoader resourceLoader;
+	@Autowired
 	private TwitterStream twitterStream;
 	@Autowired
 	private MongoTemplate mongo;
-		
+
 	@PostConstruct
 	public void run() {
 		twitterStream.addListener(new StatusListener() {
 			public void onStatus(Status status) {
 				mongo.insert(status);
-	        }
+			}
 
 			@Override
 			public void onException(Exception arg0) {
-								
 			}
 
 			@Override
 			public void onDeletionNotice(StatusDeletionNotice arg0) {
-							
 			}
 
 			@Override
 			public void onScrubGeo(long arg0, long arg1) {
-								
 			}
 
 			@Override
 			public void onStallWarning(StallWarning arg0) {
-								
 			}
 
 			@Override
 			public void onTrackLimitationNotice(int arg0) {
-								
-			}			
+			}
 		});
-		
-	    FilterQuery filter=new FilterQuery();
-	    filter.track(new String[]{"futbol"});
-	    filter.language(new String[]{"es"});
-	    twitterStream.filter(filter);
+		String[] bow=null;
+		try {
+			Resource resource=resourceLoader.getResource("classpath:bagofwords.txt");
+			Scanner sc=new Scanner(resource.getFile());
+			List<String> lines=new ArrayList<String>();
+			while (sc.hasNextLine()) {
+			  lines.add(sc.nextLine());
+			}
+			bow=lines.toArray(new String[0]);
+			sc.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		FilterQuery filter = new FilterQuery();
+		filter.track(bow);
+		filter.language(new String[]{"es"});
+		twitterStream.filter(filter);
 	}
 
 	public TwitterStream getTwitterStream() {
